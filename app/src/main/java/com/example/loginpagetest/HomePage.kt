dc4f2a6caf799c71b8cd7a2969d90a6059e10898
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2023-12-03 22:23:21 +0300

    Changed loginPagePreview.png started designing the home page

--- app/src/main/java/com/example/loginpagetest/HomePage.kt
+++ app/src/main/java/com/example/loginpagetest/HomePage.kt
@@ -1,23 +1,26 @@
 package com.example.loginpagetest
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.loginpagetest.ui.theme.LoginPageTestTheme
+import com.example.loginpagetest.ui.theme.Orange
 
 @Preview(showBackground = true)
 @Composable
@@ -44,7 +47,62 @@ fun HomePage(
                     )
                 )
             ),
-        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row (
+            horizontalArrangement = Arrangement.End
+        ){
+            Surface(
+                modifier = Modifier.size(412.dp,75.dp),
+                shape = RectangleShape,
+                color = Orange,
+            ) {
+                Row (
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Center
+                ){
+                    Surface (
+                        modifier = Modifier.size(75.dp),
+                        shape = RectangleShape,
+                        color = Color.Red,
+                    ){
+
+                    }
+                    Spacer(modifier = Modifier.width(25.dp))
+                    Surface (
+                        modifier = Modifier.size(75.dp),
+                        shape = RectangleShape,
+                        color = Color.Red,
+                    ){
+
+                    }
+                }
+            }
+
+        }
+        Spacer(modifier = Modifier.height(25.dp))
+        Surface(
+            modifier = Modifier.size(150.dp),
+        ) {
+        }
+
+    }
+
+
+
+
+    /*Column (
+        modifier = Modifier
+            .size(412.dp, 732.dp) //
+            .background(
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.White,
+                        Color.Blue
+                    )
+                )
+            ),
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.End,
     )
     {
@@ -55,5 +113,17 @@ fun HomePage(
                 .size(75.dp)
                 .shadow(1.dp, CircleShape),
         )
-    }
+        Surface(
+            modifier = Modifier
+                .size(
+                    100.dp
+                ),
+            color = Color.Red,
+        ) {
+
+        }
+
+
+
+    }*/
 }
