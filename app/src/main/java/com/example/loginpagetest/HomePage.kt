ff5a5f074103d125ea3a76a5bf495cb2c1d07918
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2023-12-12 16:51:55 +0300

    AppBar added to HomePage, Navigation added Home Page to Login Page,

--- app/src/main/java/com/example/loginpagetest/HomePage.kt
+++ app/src/main/java/com/example/loginpagetest/HomePage.kt
@@ -1,26 +1,27 @@
 package com.example.loginpagetest
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.loginpagetest.ui.theme.LoginPageTestTheme
-import com.example.loginpagetest.ui.theme.Orange
 
 @Preview(showBackground = true)
 @Composable
@@ -29,7 +30,6 @@ fun HomePagePreview() {
         HomePage(navController =  rememberNavController())
     }
 }
-
 
 @Composable
 fun HomePage(
@@ -47,83 +47,50 @@ fun HomePage(
                     )
                 )
             ),
-        horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Row (
-            horizontalArrangement = Arrangement.End
-        ){
-            Surface(
-                modifier = Modifier.size(412.dp,75.dp),
-                shape = RectangleShape,
-                color = Orange,
-            ) {
-                Row (
-                    modifier = Modifier,
-                    horizontalArrangement = Arrangement.Center
-                ){
-                    Surface (
-                        modifier = Modifier.size(75.dp),
-                        shape = RectangleShape,
-                        color = Color.Red,
-                    ){
-
-                    }
-                    Spacer(modifier = Modifier.width(25.dp))
-                    Surface (
-                        modifier = Modifier.size(75.dp),
-                        shape = RectangleShape,
-                        color = Color.Red,
-                    ){
-
-                    }
-                }
-            }
-
-        }
-        Spacer(modifier = Modifier.height(25.dp))
-        Surface(
-            modifier = Modifier.size(150.dp),
-        ) {
-        }
-
+        AppBar(navController = navController)
     }
+}
 
-
-
-
-    /*Column (
+@Composable
+fun AppBar(
+    navController : NavController
+)
+{
+    Row(
         modifier = Modifier
-            .size(412.dp, 732.dp) //
+            .size(412.dp, 70.dp)
             .background(
-                brush = Brush.verticalGradient(
-                    listOf(
-                        Color.White,
-                        Color.Blue
-                    )
-                )
+                Color.Red
             ),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.End,
-    )
-    {
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.trakyalogo),
-            contentDescription = "Image",
+            painter = painterResource(id = R.drawable.cutecat),
+            contentDescription = "image_cute_cat",
             modifier = Modifier
                 .size(75.dp)
                 .shadow(1.dp, CircleShape),
+            contentScale = ContentScale.FillBounds,
         )
-        Surface(
-            modifier = Modifier
-                .size(
-                    100.dp
-                ),
-            color = Color.Red,
+        Text(
+            text = "Home Page"
+        )
+        ElevatedButton(
+            modifier = Modifier,
+
+            onClick = { logOut(navController) }
         ) {
-
+            Text(
+                text = "Logout"
+            )
         }
+    }
+}
 
 
-
-    }*/
+fun logOut(navControl: NavController)
+{
+    navControl.navigate(route = Screen.Login.route)
 }
